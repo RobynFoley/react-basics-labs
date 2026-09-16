e38875057a2181c0d37850b6a306b7454837d34b
robynfoley <robynfoleyvf@gmail.com>
2026-09-16 13:44:28 +0100

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -7,24 +7,31 @@ function App() {
 
     const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "low" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "medium" },
-      { id: 3, title: "Tidy up", deadline: "Today", priority: "high" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "low", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "medium", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "high", done: false  }
     ]
   });
 
+    const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+  }
 
     return (
     <div className="container">
       <h1>Tasky</h1>
-        {taskState.tasks.map((task) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-      priority={task.priority}
-    />
+        {taskState.tasks.map((task, index) => (              
+          <Task 
+            title={task.title}
+            description={task.description}
+            deadline={task.deadline}
+            key={task.id}
+            priority={task.priority}
+            done={task.done}
+            markDone={() => doneHandler(index)}
+          />
   ))}
 
 
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,15 +1,13 @@
 const Task = (props) => {
-    
-           return (
-        <div className="card">
-            <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p className="description">{props.description}</p>
-            <p><b>{props.priority}</b></p>
-        </div>
+        return (
+             <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+                <p className="title">{props.title}</p>
+                <p>Due: {props.deadline}</p>
+                <p className="description">{props.description}</p>
+                <p><b>{props.priority}</b></p>
+                <button onClick={props.markDone} className='doneButton'>Done</button>
+            </div>
     )
-
-
 }
 
 export default Task;
